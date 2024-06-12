d8bc4421becf28a46a282389a7f554a76470fea1
SREE GUNASEKAR <gunasubramani2001@gmail.com>
2024-06-12 13:48:13 +0530

    FINAL COMMIT

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,15 @@ import { motion } from "framer-motion";
 const About = () => {
   return (
     <div className=" border-b border-neutral-900 pb-4">
-      <h2 className=" my-20 text-center text-4xl">
+      <motion.h2
+        whileInView={{ y: 0, opacity: 1 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className=" my-20 text-center text-4xl"
+      >
         About
         <span className=" text-neutral-500"> Me</span>
-      </h2>
+      </motion.h2>
       <div className="flex flex-wrap">
         <motion.div
           whileInView={{ x: 0, opacity: 1 }}
